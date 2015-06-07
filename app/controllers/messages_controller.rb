469b5bb3bcafd8d526c6d59c7dc90c7a44ff81a7
Ray Cheung <ray.swc@gmail.com>
2015-06-07 19:04:50 +0800

    Added `client-ref` for outbound messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     nexmo_outbound = NexmoDeliveryReceipt.last_14_days.delivered.where(msisdn: params[:mobile])
     nexmo_inbound = NexmoInboundMessage.last_14_days.where(msisdn: params[:mobile])
 
-    @nexmo_messages = (nexmo_outbound.map { |dr| { message_timestamp: dr.message_timestamp, type: "outbound", body: dr.body } } +
+    @nexmo_messages = (nexmo_outbound.map { |dr| { message_timestamp: dr.message_timestamp, type: "outbound", body: dr.body, ref: dr.client_ref } } +
                        nexmo_inbound.map { |im| { message_timestamp: im.message_timestamp, type: "inbound", body: im.text } }).
                       sort_by! { |m| [m["message_timestamp"], m["type"]] }
   rescue ActionController::ParameterMissing
--- app/views/messages/index.json.jbuilder
+++ app/views/messages/index.json.jbuilder
@@ -1,3 +1,4 @@
 json.array! @nexmo_messages do |nexmo_message|
   json.(nexmo_message, :message_timestamp, :type, :body)
+  json.ref nexmo_message[:ref] if nexmo_message[:ref]
 end
