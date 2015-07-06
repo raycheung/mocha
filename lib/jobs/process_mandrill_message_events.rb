2a35e3f5d982315ae6dcdf04026ad6b9a7954bde
Ray Cheung <ray.swc@gmail.com>
2015-07-06 20:33:11 +0800

    Fixed the issue for E11000 error
    - Mandrill may send the same event more than once

--- lib/jobs/process_mandrill_message_events.rb
+++ lib/jobs/process_mandrill_message_events.rb
@@ -2,6 +2,6 @@ class ProcessMandrillMessageEvents < Struct.new(:mandrill_message_event_request_
   def perform
     mandrill_request = MandrillMessageEventRequest.find(mandrill_message_event_request_id)
     events = JSON.parse(mandrill_request.mandrill_events)
-    events.each { |event| MandrillMessageEvent.create(event) }
+    events.each { |event| MandrillMessageEvent.new(event).upsert }
   end
 end
